9a20bbb6bee68ff9effbc2bdb9ac01613602b80d
Gann Boon Bay <gannboonbay1@gmail.com>
2016-07-18 00:27:32 -0700

    modified setup_project.py to allow scrubbing of symlinks

--- setup_project.py
+++ setup_project.py
@@ -18,7 +18,11 @@ if os.name == 'nt':
 				raise ctypes.WinError()
 		os.symlink = symlink_ms
 
+scrub =None
+
 file_path = sys.argv[0]
+if len(sys.argv)==2:
+	scrub = sys.argv[1]
 
 host_folder = os.path.dirname(file_path)
 expected_folders = ['Assets', 'sequences']
@@ -32,62 +36,100 @@ if len(dirs) == len(expected_folders):
 		# Linking Tool Folder
 		tool_source = os.path.join(host_folder, 'Assets', 'Tools')
 		tool_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Tools')
-		if not os.path.exists(tool_destination):
+		if os.path.exists(tool_destination):
+			if scrub:
+				os.rmdir(tool_destination)
+			else:
+				pass
+		else:
 			os.symlink(tool_source, tool_destination) 	
 		
 		# Linking Textures Folder
 		texture_source = os.path.join(host_folder, 'Assets', 'Textures')
 		texture_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Textures')
-		if not os.path.exists(tool_destination):
-			os.symlink(texture_source, texture_destination) 
+		if os.path.exists(texture_destination):
+			if scrub:
+				os.rmdir(texture_destination)
+			else:
+				pass
+		else:
+			os.symlink(texture_source, texture_destination)
 
 		# Linking FBX
 		setdressing_source = os.path.join(host_folder, 'Assets', 'SetDressing', 'Dead')
 		setdressing_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'SetDressing')
-		if not os.path.exists(setdressing_destination):
+		if os.path.exists(setdressing_destination):
+			if scrub:
+				os.rmdir(setdressing_destination)
+			else:
+				pass
+		else:
 			os.symlink(setdressing_source, setdressing_destination)
-
 		# Linking master with shots
 		if not shot == 't01_master':
 			# Linking Anim and Cam alembics
 			if not shot == 't01_test':
 				layoutanim_source = os.path.join(host_folder, 'Assets', 'Layout', 't01', 'abc', shot)
 				layoutanim_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Layout')
-				if not os.path.exists(layoutanim_destination):
+				if os.path.exists(layoutanim_destination):
+					if scrub:
+						os.rmdir(layoutanim_destination)
+					else:
+						pass
+				else:
 					os.symlink(layoutanim_source, layoutanim_destination)
 
 			prefabs_source = os.path.join(host_folder, 'sequences', 't01_master','Assets', 'Resources', 'Prefabs')	
 			prefabs_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Resources', 'Prefabs', 't01_master')
-			if not os.path.exists(prefabs_destination):
-				if not os.path.exists(os.path.dirname(prefabs_destination)):
-					os.makedirs(os.path.dirname(prefabs_destination))
+			if os.path.exists(prefabs_destination):
+				if scrub:
+					os.rmdir(prefabs_destination)
+				else:
+					pass
+			else:
 				os.symlink(prefabs_source, prefabs_destination)
 
 			materials_source = os.path.join(host_folder, 'sequences', 't01_master','Assets', 'Materials')	
 			materials_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Materials', 't01_master')
-			if not os.path.exists(materials_destination):
-				if not os.path.exists(os.path.dirname(materials_destination)):
-					os.makedirs(os.path.dirname(materials_destination))
+			if os.path.exists(materials_destination):
+				if scrub:
+					os.rmdir(materials_destination)
+				else:
+					pass
+			else:
 				os.symlink(materials_source, materials_destination)
 
 			if not shot == 't01_test':
 				scene_source = os.path.join(host_folder, 'sequences', 't01_master','Assets', 'Scene')
 				scene_destination = os.path.join(host_folder, 'sequences', shot, 'Assets', 'Scene', 't01_master')
-				if not os.path.exists(scene_destination):
-					if not os.path.exists(os.path.dirname(scene_destination)):
-						os.makedirs(os.path.dirname(scene_destination))
+				if os.path.exists(scene_destination):
+					if scrub:
+						os.rmdir(scene_destination)
+					else:
+						pass
+				else:
 					os.symlink(scene_source, scene_destination)
 		
 			tagmanager_source = os.path.join(host_folder, 'sequences', 't01_master', 'ProjectSettings', 'TagManager.asset')
 			tagmanager_destination = os.path.join(host_folder, 'sequences', shot, 'ProjectSettings', 'TagManager.asset')
-			if not os.path.exists(tagmanager_destination):
+			if os.path.exists(tagmanager_destination):
+				if scrub:
+					os.remove(tagmanager_destination)
+				else:
+					pass
+			else:
 				if not os.path.exists(os.path.dirname(tagmanager_destination)):
 					os.makedirs(os.path.dirname(tagmanager_destination))
 				os.symlink(tagmanager_source, tagmanager_destination)
 
 			projectsettings_source = os.path.join(host_folder, 'sequences', 't01_master', 'ProjectSettings', 'ProjectSettings.asset')
 			projectsettings_destination = os.path.join(host_folder, 'sequences', shot, 'ProjectSettings', 'ProjectSettings.asset')
-			if not os.path.exists(projectsettings_destination):
+			if os.path.exists(projectsettings_destination):
+				if scrub:
+					os.remove(projectsettings_destination)
+				else:
+					pass
+			else:
 				if not os.path.exists(os.path.dirname(projectsettings_destination)):
 					os.makedirs(os.path.dirname(projectsettings_destination))
 				os.symlink(projectsettings_source, projectsettings_destination)
